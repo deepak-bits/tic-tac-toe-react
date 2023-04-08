e9563f3484d92e553ecef066b82601eacc0081aa
deepak-bits <deepak.kumar3112@gmail.com>
2023-04-09 00:55:05 +0530

    Updates

---
+++ precache-manifest.7938dd8199cb66e54330fa6b0e9de60e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0466533623006b01d345",
+    "url": "/tic-tac-toe-react/static/css/main.1ae02654.chunk.css"
+  },
+  {
+    "revision": "0466533623006b01d345",
+    "url": "/tic-tac-toe-react/static/js/main.04665336.chunk.js"
+  },
+  {
+    "revision": "ed720ef6f526bf6ca7b0",
+    "url": "/tic-tac-toe-react/static/js/1.ed720ef6.chunk.js"
+  },
+  {
+    "revision": "24a1bc39042ecf940668",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.24a1bc39.js"
+  },
+  {
+    "revision": "80a3a482f70ee0326ef157a7b6c9c193",
+    "url": "/tic-tac-toe-react/index.html"
+  }
+];
